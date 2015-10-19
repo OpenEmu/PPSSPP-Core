8925ad66aeaa497f5de50caa3eaaaee24e8c553b
Alexander Strange <astrange@ithinksw.com>
2015-10-18 17:40:22 -0700

    Pretty new OEGameCore refactor in preparation for OpenGL3 support. Should not break backwards compatibility.

--- PPSSPPGameCore.mm
+++ PPSSPPGameCore.mm
@@ -123,9 +123,6 @@
 
 - (void)stopEmulation
 {
-    // We need a OpenGL context here, because PPSSPP cleans up framebuffers at this point
-    [[self renderDelegate] willRenderOnAlternateThread];
-    [[self renderDelegate] startRenderingOnAlternateThread];
     PSP_Shutdown();
 
     NativeShutdownGraphics();
