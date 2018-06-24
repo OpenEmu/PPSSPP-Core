26eec3c11358d7bafb9ee65a716b950848f862bc
Duckey77 <duckey77@live.com>
2018-06-24 12:20:08 -0700

    Enable fast forward with new threaded rendering

--- PPSSPPGameCore.mm
+++ PPSSPPGameCore.mm
@@ -202,6 +202,9 @@ PPSSPPGameCore *_current = 0;
         NativeSetThreadState(OpenEmuCoreThread::EmuThreadState::START_REQUESTED);
         
     } else {
+        //If Fast forward rate is detected, unthrottle the rndering
+        PSP_CoreParameter().unthrottle = (self.rate == 5) ? true : false;
+
         //Let PPSSPP Core run a loop and return
         UpdateRunLoop();
     }
