3d2c57a06f7d734dc6ee58401953d240543a10b2
Duckey77 <duckey77@live.com>
2023-01-25 17:13:43 -0700

    Update NativeApp for changes in ppsspp 1.14.x

--- NativeApp.cpp
+++ NativeApp.cpp
@@ -1,3 +1,4 @@
+
 /*
  Copyright (c) 2013, OpenEmu Team
 
@@ -32,11 +33,13 @@
 #include "Common/CPUDetect.h"
 
 #include "Core/Core.h"
+#include "Core/Config.h"
 #include "Core/CoreTiming.h"
 #include "Core/Host.h"
 #include "Core/System.h"
 #include "Core/HLE/__sceAudio.h"
 #include "Core/ThreadPools.h"
+#include "Core/FileLoaders/DiskCachingFileLoader.h"
 
 #include "File/VFS/VFS.h"
 #include "File/VFS/AssetReader.h"
@@ -50,6 +53,7 @@
 
 #include "GPU/GPUState.h"
 #include "GPU/GPUInterface.h"
+#include "Common/GPU/ShaderTranslation.h"
 
 #include "DataFormatGL.h"
 
@@ -78,9 +82,13 @@ inline const char *removePath(const char *str) {
 KeyInput input_state;
 OnScreenMessages osm;
 
+static Draw::DrawContext *g_draw;
+static Draw::Pipeline *colorPipeline;
+static Draw::Pipeline *texColorPipeline;
+
 namespace OpenEmuCoreThread {
     OpenEmuGLContext *ctx;
-    
+
     enum class EmuThreadState {
         DISABLED,
         START_REQUESTED,
@@ -90,33 +98,34 @@ namespace OpenEmuCoreThread {
         QUIT_REQUESTED,
         STOPPED,
     };
-    
+
     static std::thread emuThread;
     static bool threadStarted = false;
     static std::atomic<EmuThreadState> emuThreadState(EmuThreadState::DISABLED);
-    
+
     static void EmuFrame() {
+
         ctx->SetRenderTarget();
-        
+
         if (ctx->GetDrawContext()) {
             ctx->GetDrawContext()->BeginFrame();
         }
-        
+
         gpu->BeginHostFrame();
-        
+
         coreState = CORE_RUNNING;
         PSP_RunLoopUntil(UINT64_MAX);
-        
+
         gpu->EndHostFrame();
-        
+
         if (ctx->GetDrawContext()) {
             ctx->GetDrawContext()->EndFrame();
         }
     }
-    
+
     static void EmuThreadFunc() {
 		SetCurrentThreadName("Emu");
-        
+
         while (true) {
             switch ((EmuThreadState)emuThreadState) {
                 case EmuThreadState::START_REQUESTED:
@@ -140,24 +149,24 @@ namespace OpenEmuCoreThread {
             }
         }
     }
-    
+
     void EmuThreadStart() {
         bool wasPaused = emuThreadState == EmuThreadState::PAUSED;
         emuThreadState = EmuThreadState::START_REQUESTED;
-        
+
         if (!wasPaused) {
             ctx->ThreadStart();
             emuThread = std::thread(&EmuThreadFunc);
         }
     }
-    
+
     void EmuThreadStop() {
         if (emuThreadState != EmuThreadState::RUNNING) {
             return;
         }
 
         emuThreadState = EmuThreadState::QUIT_REQUESTED;
-        
+
         while (ctx->ThreadFrame()) {
             // Need to keep eating frames to allow the EmuThread to exit correctly.
             continue;
@@ -166,13 +175,13 @@ namespace OpenEmuCoreThread {
         emuThread = std::thread();
         ctx->ThreadEnd();
     }
-    
+
     void EmuThreadPause() {
         if (emuThreadState != EmuThreadState::RUNNING) {
             return;
         }
         emuThreadState = EmuThreadState::PAUSE_REQUESTED;
-        
+
         while (emuThreadState != EmuThreadState::PAUSED) {
             //We need to process frames until the thread Pauses give 10 ms between loops
             ctx->ThreadFrame();
@@ -255,19 +264,38 @@ int NativeMix(short *audio, int num_samples)
 
 	return num_samples;
 }
+bool CreateGlobalPipelines();
 
 void NativeInit(int argc, const char *argv[], const char *savegame_directory, const char *external_directory, const char *cache_directory)
 {
     VFSRegister("", new DirectoryAssetReader(Path("assets/")));
     VFSRegister("", new DirectoryAssetReader(Path(external_directory)));
-    
+
+    ShaderTranslationInit();
+
 	g_threadManager.Init(cpu_info.num_cores, cpu_info.logical_cpu_count);
 
+    g_Config.defaultCurrentDirectory = Path(external_directory);
+    g_Config.memStickDirectory = g_Config.defaultCurrentDirectory / "config/ppsspp";
+    g_Config.flash0Directory = Path(std::string(external_directory)) / "flash0";
+    
+    if (cache_directory && strlen(cache_directory)) {
+        g_Config.appCacheDirectory = Path(cache_directory);
+        DiskCachingFileLoaderCache::SetCacheDir(g_Config.appCacheDirectory);
+    }
+    
+    g_Config.bEnableLogging=true;
+    
+    if (!LogManager::GetInstance()) {
+        LogManager::Init(&g_Config.bEnableLogging);
+    }
+    
+    g_Config.SetSearchPath(GetSysDirectory(DIRECTORY_SYSTEM));
+    g_Config.Load();
+    
     if (host == nullptr) {
         host = new NativeHost();
     }
-    
-    logger = new AndroidLogger();
 
 	LogManager *logman = LogManager::GetInstance();
 	ILOG("Logman: %p", logman);
@@ -293,16 +321,19 @@ bool NativeInitGraphics(GraphicsContext *graphicsContext)
 {
     //Set the Core Thread graphics Context
     OpenEmuCoreThread::ctx = static_cast<OpenEmuGLContext*>(graphicsContext);
-    
+
     // Save framebuffer and set ppsspp default graphics framebuffer object
     glGetIntegerv(GL_FRAMEBUFFER_BINDING_EXT, &framebuffer);
     OpenEmuCoreThread::ctx->SetRenderFBO(framebuffer);
- 
+
     Core_SetGraphicsContext(OpenEmuCoreThread::ctx);
-    
+    g_draw = graphicsContext->GetDrawContext();
+
+    CreateGlobalPipelines();
+
     if (gpu)
         gpu->DeviceRestore();
-    
+
     return true;
 }
 
@@ -312,12 +343,40 @@ void NativeRender(GraphicsContext *ctx)
 {
     if(OpenEmuCoreThread::emuThreadState == OpenEmuCoreThread::EmuThreadState::PAUSED)
         return;
-    
+
     OpenEmuCoreThread::ctx->ThreadFrame();
     OpenEmuCoreThread::ctx->SwapBuffers();
 }
 
 void NativeUpdate() {}
+
+bool CreateGlobalPipelines() {
+    using namespace Draw;
+
+    ShaderModule *vs_color_2d = g_draw->GetVshaderPreset(VS_COLOR_2D);
+    ShaderModule *fs_color_2d = g_draw->GetFshaderPreset(FS_COLOR_2D);
+    ShaderModule *vs_texture_color_2d = g_draw->GetVshaderPreset(VS_TEXTURE_COLOR_2D);
+    ShaderModule *fs_texture_color_2d = g_draw->GetFshaderPreset(FS_TEXTURE_COLOR_2D);
+
+    if (!vs_color_2d || !fs_color_2d || !vs_texture_color_2d || !fs_texture_color_2d) {
+        ERROR_LOG(G3D, "Failed to get shader preset");
+        return false;
+    }
+
+    //InputLayout *inputLayout = ui_draw2d.CreateInputLayout(g_draw);
+    BlendState *blendNormal = g_draw->CreateBlendState({ true, 0xF, BlendFactor::SRC_ALPHA, BlendFactor::ONE_MINUS_SRC_ALPHA });
+    DepthStencilState *depth = g_draw->CreateDepthStencilState({ false, false, Comparison::LESS });
+    RasterState *rasterNoCull = g_draw->CreateRasterState({});
+
+    // Release these now, reference counting should ensure that they get completely released
+    // once we delete both pipelines.
+   
+    rasterNoCull->Release();
+    blendNormal->Release();
+    depth->Release();
+    return true;
+}
+
 
 void NativeShutdownGraphics()
 {
